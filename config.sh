eab5950b849304bf0a2158aeeb2b632998a534e9
Kartik Banwala <KartikbanwalaOP@gmail.com>
2022-09-11 14:27:37 +0530

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Git Configuration
-export GIT_USERNAME="Sushrut1101"
-export GIT_EMAIL="guptasushrut@gmail.com"
+export GIT_USERNAME="Kartik-91"
+export GIT_EMAIL="kartikbanwalaop@gmail.com"
 
 git config --global user.name "${GIT_USERNAME}"
 git config --global user.email "${GIT_EMAIL}"
